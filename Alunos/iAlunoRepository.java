f8628c041911d36bf05352d4d0009b0fe8618efa
Antônio Alexandre Sobral de Oliveira Cavalcante <127903871+Toin19@users.noreply.github.com>
2023-12-05 21:49:03 -0300

    Delete Alunos directory

--- Alunos/iAlunoRepository.java
+++
@@ -1,7 +0,0 @@
-import java.util.List;
-public interface iAlunoRepository {
-   void adicionar(Aluno aluno);
-   void remover(Aluno aluno);
-   Aluno buscar(String nome);
-   List<Aluno> listar();
-}
